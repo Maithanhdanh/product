301b31f8fc27cb4e428e7efa376e51398c8da224
Mai Thanh Danh <mtd.maithanhdanh@gmail.com>
2020-11-18 21:35:49 +0700

    [UPDATE] get local server IP

--- helpers/remoteIP.js
+++ helpers/remoteIP.js
@@ -3,21 +3,27 @@
 const { networkInterfaces } = require("os")
 
 const nets = networkInterfaces()
-var results = [] // or just '{}', an empty object
+var results = {} // or just '{}', an empty object
 
 for (const name of Object.keys(nets)) {
-	if (name === "WiFi" || name === "Ethernet") {
-		for (const net of nets[name]) {
-			// skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
-			if (net.family === "IPv4" && !net.internal) {
-				if (!results[name]) {
-					results = []
-				}
-
-				results.push(net.address)
+	for (const net of nets[name]) {
+		// skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
+		if (net.family === "IPv4" && !net.internal) {
+			if (!results[name]) {
+				results[name] = []
 			}
+
+			results[name].push(net.address)
 		}
 	}
 }
+var result
+if(results.eth0) {
+	result = results.eth0
+} else if(results.WiFi){
+	result = results.WiFi
+} else {
+	result = results.Ethernet
+}
 
-module.exports = results[0]
+module.exports = result[0]
